c509c0c4cacad6a220d52c417d897c4f467f3eea
edgar <seo@disoin.com>
2025-02-21 09:03:23 -0600

    Se actualiza codigo con try-catch para el controlador

--- controllers/articulo.js
+++ controllers/articulo.js
@@ -1,3 +1,6 @@
+const { status } = require('express/lib/response');
+const validator = require('validator')
+
 const prueba = (req, res) => {
 
     return res.status(200).json({
@@ -8,6 +11,20 @@ const prueba = (req, res) => {
 const create = (req, res) => {
     // Recoger los parametros por el metodo post
     let parametros = req.body;
+
+    try {
+      let validar_titulo = !validator.isEmpty(parametros.titulo); //variable para validar que el campo titulo no esta vacio
+      let validar_contenido = !validator.isEmpty(parametros.contenido); //variable para validar que el campo contenido no esta vacio
+      
+      if(!validar_titulo || !validar_contenido){ //Se evalua si alguna variable es true, si alguno de los dos es true es por que esta vacio y lanza el mensaje de error
+        throw new Error('Informacion no valida');// Mensaje de error a lanzar
+      }
+    } catch (error) {
+        return res.status(400).json({
+            status: "Error",
+            mensaje: "Faltan datos por enviar"
+        });
+    }
     
     // Validar datos
 
@@ -19,7 +36,8 @@ const create = (req, res) => {
 
     // Devolver el resultado
     return res.status(200).json({
-        mensaje: "Respuesta del controlador para crear"
+        mensaje: "Respuesta del controlador para crear",
+        parametros
     })
 }
 
